88eb50d7d3752146f14923a9271dc974934e09cd
Xurde-dip <dipeshbh2001@gmail.com>
2026-03-06 11:55:29 +0545

    chunk size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          supabase: ['@supabase/supabase-js'],
+          ui: ['framer-motion', 'lucide-react'],
+        },
+      },
+    },
+  },
 })
